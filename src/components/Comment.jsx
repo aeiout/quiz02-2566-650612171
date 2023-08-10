565bdac0c45a0e528d13fb7b71d0f14fd6095689
Your Name <ueaarthon13117@gmail.com>
2023-08-10 15:36:15 +0700

    finish

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,7 @@
+import { comments } from "@/libs/comments";
+import { Reply } from "./Reply";
+import { useState } from "react";
+const [isLikeShown,setIsLikeShown]= useState(false);
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +9,48 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+
+  
+  
+  if(likeNum !== 0) {
+  setIsLikeShown=(!isLikeShown);
+}
   return (
     <div>
-      your code here...
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {username}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span>
+          </div>
+        </div>
+      </div>
+      <Reply/>
       {/* map-loop render Reply component here */}
+      {/* { replies.map((Reply) => (
+        <Reply
+          key ={Reply.username}
+          userImagePath={Reply.userImagePath}
+          username={Reply.username}
+          commentText={Reply.commentText}
+          likeNum={Reply.likeNum}
+        />
+      ))} */}
     </div>
   );
 };
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,27 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+export const PostOwner = (props) => {
+  return <div className="vstack gap-3">
+  <div className="d-flex align-items-center gap-3">
+    <img
+      src="/profileImages/ueaarthorn.jpg"
+      width="48"
+      height="48"
+      className="rounded-circle"
+      style={{ objectFit: "cover" }}
+    />
+    <span className="fw-semibold fs-5 text-white">
+      {props.user}
+    </span>
+  </div>
+  
+
+  <span className="text-white">
+    {props.text}
+  </span>
+
+  <div className="d-flex align-items-center gap-1">
+    <img src="/like.svg" width={20}></img>
+    <span style={{ color: "#B0B3B8" }}>{props.liked}</span>
+  </div>
+  <hr className="m-0 border" />
+</div>
 };
